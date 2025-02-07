2c093c80c34dd4a5fc37d03c8fe7ca3e860fb135
nurulafsarinfo <mdnurulafsar123afsar@gmail.com>
2025-02-07 14:48:19 +0600

    added task 05 solution

--- task04.js
+++ task04.js
@@ -1,27 +1,28 @@
-// let score = 0;
-// let frndScore = 0;
+let score = 0;
+let frndScore = 0;
 
-// if(score > 80){
-//     if(frndScore > 80){
-//         console.log("Go for lunch")
-//     }
-//     else if(frndScore < 80 && frndScore >= 60){
-//         console.log("Good luck next time!")
-//     }
-//     else if(frndScore < 60 && frndScore >= 40){
-//         console.log("Keep your friend's mssg Unseen!")
-//     }
-//     else{
-//         console.log("Block your friend")
-//     }
-// }
-// else{
-//     console.log("Go to home and Sleep and act sad!")
-// }
+if(score > 80){
+    if(frndScore > 80){
+        console.log("Go for lunch")
+    }
+    else if(frndScore < 80 && frndScore >= 60){
+        console.log("Good luck next time!")
+    }
+    else if(frndScore < 60 && frndScore >= 40){
+        console.log("Keep your friend's mssg Unseen!")
+    }
+    else{
+        console.log("Block your friend")
+    }
+}
+else{
+    console.log("Go to home and Sleep and act sad!")
+}
 
 // shorthand if-else
-let score = 39;
-let frndScore = 30;
+
+// let score = 39;
+// let frndScore = 30;
 
 score > 80 
     ? console.log("Go for lunch") 
---
+++ task05.js
@@ -0,0 +1,10 @@
+let num1 = 88;
+let num2 = 53;
+let result = num1 + num2;
+
+if (num1 > num2){
+    console.log("Result isL "+ num1*2)
+}
+else{
+    console.log("Result is: "+result)
+}
